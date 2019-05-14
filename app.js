97d56605154e1e06124e794416fddc5961ba45cb
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-05-15 00:22:26 +0600

    Update app.js
    Added feature 3: Showing price of the item if it's available in the store.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ class Store {
   constructor(name) {
     this.name = name;
     this.items = [];
+    this.prices = {};
   }
 
   isItemAvailable(name) {
@@ -13,13 +14,24 @@ class Store {
     }
   }
 
-  addItem(name) {
+  addItem(name, price) {
     this.items.push(name);
+    this.prices[name] = price;
+  }
+
+  getPrice(name) {
+    var isAvailable = this.isItemAvailable(name);
+    if (isAvailable == true) {
+      var itemPrice = this.prices[name];
+      return itemPrice;
+    } else {
+      console.log("Sorry! We don't have the item.");
+    }
   }
 }
 
 var myStore = new Store("RAR Fashion House");
-myStore.addItem("shirt");
-myStore.addItem("pant");
-myStore.isItemAvailable("shirt");
-myStore.isItemAvailable("shoes");
+myStore.addItem("shirt", 500);
+myStore.addItem("pant", 700);
+myStore.getPrice("shirt");
+myStore.getPrice("bread");
